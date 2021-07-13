6670012ce565ea90ce734374773f62a32c597347
damien.boudoire <damien.boudoire@laplateforme.io>
2021-07-13 09:56:33 +0200

    change models name

--- final/models.py
+++ final/models.py
@@ -25,7 +25,7 @@ def make_CNN(input_shape, num_classes):
     return model
 
 
-def makeCnnGru(input, output, dropout=.2):
+def makeCnnGRU(input, output, dropout=.2):
     model = Sequential()
     model.add(Conv1D(32, kernel_size=10, padding='same', activation='relu', input_shape=input))
     model.add(Conv1D(32, kernel_size=10, padding='same', activation='relu'))
@@ -57,7 +57,7 @@ def makeCnnGru(input, output, dropout=.2):
     return model
 
 
-def makeLSTMCNN(nb_class):
+def makeCnnLSTM(nb_class):
     dim=(17908, 1)#,np.shape(X_train)[1:]
 #    print(nb_class)
 

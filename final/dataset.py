99b2cc634e2c5ed78a8883c17f584eafea46bdbf
damien.boudoire <damien.boudoire@laplateforme.io>
2021-07-13 09:29:09 +0200

    init final

---
+++ final/dataset.py
@@ -0,0 +1,155 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import RandomUnderSampler
+
+
+def load_processed(target, undersample)
+    if undersample:
+        eq = 'balanced'
+    else:
+        eq = 'unbalanced'
+
+    if target == 'DeltaZ':
+        nb_class = 2
+        nom_classes = ["Deltaz<1e-3", "Deltaz>=1e-3"]
+
+    if target == 'Success2':
+        nb_class = 2
+        nom_classes = ["Success", "Spurious/Mismatch"]
+
+    if target == 'Flag3':
+        nb_class = 3
+        nom_classes = ["flags 1", "flags 2-9", "flags 3-4"]
+
+    if target == 'Success3':
+        nb_class = 3
+        nom_classes = ["Success", "Spurious", "Missmatch"]
+
+    name = '../data/' + target + '/' + eq
+    print(name)
+    X_train = np.load(name + '/TRAIN/X.npy')
+    target_train = np.load(name + '/TRAIN/Y.npy')
+
+    X_test = np.load(name + '/TEST/X.npy')
+    target_test = np.load(name + '/TEST/Y.npy')
+
+    X_validation = np.load(name + '/VALID/X.npy')
+    target_validation = np.load(name + '/VALID/Y.npy')
+
+    Y_train = tf.keras.utils.to_categorical(target_train, nb_class)  # convertir en one-hot-code
+    Y_test = tf.keras.utils.to_categorical(target_test, nb_class)
+    Y_validation = tf.keras.utils.to_categorical(target_validation, nb_class)
+
+    return X_train, X_validation, X_test, target_train, target_validation, target_test, Y_train, Y_validation, Y_test, nb_class, nom_classes
+
+
+def load_dataset(target,Undersample):
+
+  ##### Target #######
+  # Les cibles peuvent être : 'Flag' , 'Success' , 'DeltaZ'
+
+	pdf_zgrid=np.load('../data/zgrid32.npy')
+	attributes=np.load('../data/attributes32.npy', allow_pickle=True)
+	pdfs=np.load('../data/pdfs32.npy', allow_pickle=True)
+
+
+	if target == 'Flag':
+		y=np.floor(attributes[:,-1]).astype(int)
+		y=np.where(y==9, 0, y)
+
+		nom_classes = ["Flag 9", "Flag 1", "Flag 2", "Flag 3", "Flag 4"]
+
+
+	elif target == 'Success_2':
+		flag=attributes[:,5]
+		y=[]
+
+		for i in flag:
+			if i =='success':
+				nw_fl=0
+			else:
+				nw_fl=1
+
+			y.append(nw_fl)
+
+		nom_classes = ["Success", "Spurious/missmatch"]
+
+
+
+
+	elif target == 'Flag_class':
+		flag=np.floor(attributes[:,-1]).astype(int)
+		y=[]
+
+		for i in flag:
+
+			if (i == 2) or (i == 9):
+				nw_fl=1
+			elif (i == 3) or (i == 4):
+				nw_fl=2
+			elif i == 1:
+				nw_fl=0
+
+			y.append(nw_fl)
+
+		nom_classes = ["flags 1", "flags 2-9", "flags 3-4"]
+
+
+	elif target == 'DeltaZ':
+		flag=attributes[:,3]
+
+		y=[]
+
+		flag=abs(flag)
+
+		for i in flag:
+
+			if i < 5e-3:
+				nw_fl=0
+			elif i >= 5e-3:
+				nw_fl=1
+
+			y.append(nw_fl)
+
+		nom_classes= ["Deltaz<1e-3", "Deltaz>=1e-3"]
+
+	elif target == 'Success_3':
+		flag=attributes[:,5]
+		y=[]
+
+		for i in flag:
+			if i =='success':
+				nw_fl=0
+			elif i == 'spurious':
+				nw_fl=1
+			else:
+				nw_fl=2
+
+			y.append(nw_fl)
+
+		nom_classes = ["Success", "Spurious" , "Missmatch"]
+
+	else:
+		raise ValueError("Choisis la target = 'Flag' , 'Success_2' , 'Flag_class' , 'DeltaZ' , 'Sucess_3' ")
+
+
+	nb_classes=len(np.unique(y))
+
+	X_train, X_test, target_train, target_test = train_test_split(pdfs, y, test_size=0.15)
+	X_train, X_validation, target_train, target_validation = train_test_split(X_train, target_train, test_size=0.15)
+
+	if Undersample :
+		rus = RandomUnderSampler(random_state=0)
+		X_train, target_train = rus.fit_resample(X_train, target_train)
+		X_test, target_test = rus.fit_resample(X_test, target_test)
+		X_validation, target_validation = rus.fit_resample(X_validation, target_validation)
+
+
+	Y_train =  tf.keras.utils.to_categorical(target_train, nb_classes)   #convertir en one-hot-code
+	Y_test = tf.keras.utils.to_categorical(target_test, nb_classes)
+	Y_validation = tf.keras.utils.to_categorical(target_validation, nb_classes)
+
+
+
+	return X_train, X_validation ,X_test, target_train, target_validation ,target_test , Y_train , Y_validation, Y_test , nb_classes, nom_classes
---
+++ final/models.py
@@ -0,0 +1,130 @@
+from keras.models import Sequential
+from keras.layers import Conv1D, GRU, LSTM, MaxPooling1D, Dense, Flatten
+
+def make_CNN(input_shape, num_classes):
+
+    model = Sequential()
+
+    model.add(Conv1D(filters = 32, kernel_size = 9, activation = 'relu', input_shape = input_shape))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Conv1D(filters = 64, kernel_size = 6, activation = 'relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Conv1D(filters = 128, kernel_size = 3, activation = 'relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Flatten())
+
+    model.add(Dense(64, activation='relu'))
+
+    model.add(Dense(32, activation='relu'))
+
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+
+
+def makeCnnGruModel2(input, output, dropout=.2):
+    model = Sequential()
+    model.add(Conv1D(32, kernel_size=10, padding='same', activation='relu', input_shape=input))
+    model.add(Conv1D(32, kernel_size=10, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(64, kernel_size=6, padding='same', activation='relu'))
+    model.add(Conv1D(64, kernel_size=6, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(128, kernel_size=5, padding='same', activation='relu'))
+    model.add(Conv1D(128, kernel_size=5, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(256, kernel_size=3, padding='same', activation='relu'))
+    model.add(Conv1D(256, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(GRU(512, return_sequences=True))
+    model.add(GRU(256, return_sequences=True))
+    model.add(GRU(128, return_sequences=True))
+    model.add(GRU(64))
+    model.add(Dropout(dropout))
+    model.add(Flatten())
+
+    # Output layer
+    if output > 2:
+        print("Model Categorical")
+        model.add(Dense(units=output, activation="softmax"))
+    else:
+        print("Model Binary")
+        model.add(Dense(units=1, activation="sigmoid"))
+    return model
+
+
+def makeCnnGruModel2(input, output, dropout=.2):
+    model = Sequential()
+    model.add(Conv1D(32, kernel_size=10, padding='same', activation='relu', input_shape=input))
+    model.add(Conv1D(32, kernel_size=10, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(64, kernel_size=6, padding='same', activation='relu'))
+    model.add(Conv1D(64, kernel_size=6, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(128, kernel_size=5, padding='same', activation='relu'))
+    model.add(Conv1D(128, kernel_size=5, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(256, kernel_size=3, padding='same', activation='relu'))
+    model.add(Conv1D(256, kernel_size=3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(GRU(512, return_sequences=True))
+    model.add(GRU(256, return_sequences=True))
+    model.add(GRU(128, return_sequences=True))
+    model.add(GRU(64))
+    model.add(Dropout(dropout))
+    model.add(Flatten())
+
+    # Output layer
+    if output > 2:
+        print("Model Categorical")
+        model.add(Dense(units=output, activation="softmax"))
+    else:
+        print("Model Binary")
+        model.add(Dense(units=1, activation="sigmoid"))
+    return model
+
+
+def LSTM_CNN_redshift(nb_class):
+    dim=(17908, 1)#,np.shape(X_train)[1:]
+#    print(nb_class)
+
+    if nb_class == 2:
+      loss='binary_crossentropy'
+      last_activation='Softmax'
+    else :
+      loss='categorical_crossentropy'
+      last_activation='Softmax'
+
+#    print(loss)
+
+    model= Sequential()
+    model.add(Conv1D(32,kernel_size=10,padding='same', activation='relu', input_shape=dim))
+    model.add(Conv1D(32,kernel_size=10,padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Conv1D(64,kernel_size=6,padding='same', activation='relu'))
+    model.add(Conv1D(64,kernel_size=6,padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Conv1D(128,kernel_size=5,padding='same',activation='relu'))
+    model.add(Conv1D(128,kernel_size=5,padding='same', activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Conv1D(256,kernel_size=3,padding='same',activation='relu'))
+    model.add(Conv1D(256,kernel_size=3,padding='same',activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(LSTM(256, return_sequences=True))
+    model.add(LSTM(128, return_sequences=True))
+    model.add(LSTM(64, return_sequences=True))
+    model.add(LSTM(32))
+
+    model.add(Dense(nb_class,activation=last_activation))
+
+    return model
